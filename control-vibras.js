95a5338789202e44ff7b4aca1e41740e0188c95f
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-02-15 05:42:41 -0500

    Update control-vibras.js

--- control-vibras.js
+++ control-vibras.js
@@ -2,10 +2,25 @@
 // Estado: "activo" permite el uso / "suspendido" bloquea la App
 
 const ESTADO_CLIENTES = {
+    "distrileco": "activo",
     "distribuciones-jm": "activo",
+    "mercafacil": "activo",
     "deleite-saludable": "activo",
-    "la-barberia": "activo",
+    "bodega-la-central": "activo",
+    "fruver-lacentral": "activo",
+    "miniprecios": "activo",
+    "tienda-cielo": "activo",
+    "tienda-yire": "activo",
     "atarrayas": "activo",
+    "sotracor-caucasia": "activo",
+    "labarberia": "activo",
+    "odontologia--estetica-bocas": "activo",
+    "celumaster": "activo",
+    "edogra-distribuciones": "activo",
+    "inversiones-charlotte": "activo",
+    "luzmar": "activo",
+    "nelson": "activo",
+    "santuariolaprimera": "activo",
     "drogueria-inglesa": "activo",
     "estos-burgers": "activo",
     "paula-areiza": "activo"
